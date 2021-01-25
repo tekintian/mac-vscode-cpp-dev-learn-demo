8f391221adf0a7bb16702e75f2cd22c2e60a8f66
tekintian <tekintian@gmail.com>
2021-01-25 14:06:03 +0800

    C++ DEMO

---
+++ src/func1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+//声明函数
+int max(int num1, int num2);
+
+int main()
+{
+    int a=100, b=200;
+    int ret;
+    ret =max(a,b);
+    cout<<"Max value is:"<<ret <<endl;
+    return 0;
+}
+
+int max(int num1,int num2)
+{
+    return num1>num2? num1:num2;
+}
